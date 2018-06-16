1b49beacc5de5479e0222887f01f266749b425ef
Eva van Rooden <evavanrooden@gmail.com>
2018-06-16 15:44:05 +0200

    added ReadFileApp

---
+++ src/main/java/com/capgemini/animals/ReadFile.java
@@ -0,0 +1,21 @@
+package com.capgemini.animals;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try { FileReader file = new FileReader( "oscar.txt" );
+            BufferedReader buffer = new BufferedReader( file );
+            String line = "";
+            while ( ( line = buffer.readLine() ) != null )
+            { System.out.println( line ); }
+            buffer.close();
+        }
+        catch ( IOException e )
+        {
+            System.out.println( "A read error has occurred" );
+        }
+
+
+    }
+}
