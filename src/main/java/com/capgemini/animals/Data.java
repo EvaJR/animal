252a026e92e8a1aedc3e4a7546fbcf1a256383db
Eva van Rooden <evavanrooden@gmail.com>
2018-06-16 12:51:28 +0200

    added Multi, Data and Draw class
    Multi class uses methods and variables from Data and Draw

---
+++ src/main/java/com/capgemini/animals/Data.java
@@ -0,0 +1,13 @@
+package com.capgemini.animals;
+
+public class Data {
+    public final static String txt =
+            "This is a global variable in the Data class";
+
+    public static void greeting()
+    {
+        System.out.print( "This is a global method " );
+        System.out.println( "in the Data class" );
+    }
+
+}
---
+++ src/main/java/com/capgemini/animals/Draw.java
@@ -0,0 +1,8 @@
+package com.capgemini.animals;
+
+public class Draw {
+    static void line()
+    {
+        System.out.println( "__________________________" );
+    }
+}
